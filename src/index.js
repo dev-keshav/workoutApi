58abb1507d5772d642a88aadbd33d75ded6c09ed
dev-keshav <keshavkumar9431@gmail.com>
2023-06-29 12:37:37 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("<h2>It is cool api</h2>");    
+})
+
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+    console.log(`API is listing on port ${PORT}`);
+});
