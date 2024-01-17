73539da1fea9edd29309fa01323a44b788bd0e35
dev-keshav <keshavkumar9431@gmail.com>
2024-01-17 22:32:40 +0530

    change in index.js

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ app.listen(PORT, () => {
 
 
 app.get('*', (req, res) => {
-    res.send(`<h2> Le API response..</h2> <img height=222 width=222 src="https://memeheist.com/wp-content/uploads/2023/08/Hum-Pe-To-Hai-Hi-No-Meme-1-1024x1024.jpg" />`);    
+    res.send(`<h2> Le API response..404</h2> <img height=222 width=222 src="https://memeheist.com/wp-content/uploads/2023/08/Hum-Pe-To-Hai-Hi-No-Meme-1-1024x1024.jpg" /> <br><h3>Here you can find👇</h3> <p>https://health-and-fitness-api.onrender.com/api/v1/workouts</p>`);    
 })
