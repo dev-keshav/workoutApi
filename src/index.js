f717ad9d0c2083d9dc773d51b1ea40fcc03cdd1e
dev-keshav <keshavkumar9431@gmail.com>
2024-01-17 22:48:07 +0530

    change in first page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("<h2>It is cool api</h2> ");    
+    res.send("<h2>It is cool api</h2> <br><h3>Here you can find👇</h3> <p>https://health-and-fitness-rvrr.onrender.com/api/v1/workouts</p>");    
 })
 
 app.use(bodyparser.json());
@@ -21,5 +21,5 @@ app.listen(PORT, () => {
 
 
 app.get('*', (req, res) => {
-    res.send(`<h2> Le API response..404</h2> <img height=222 width=222 src="https://memeheist.com/wp-content/uploads/2023/08/Hum-Pe-To-Hai-Hi-No-Meme-1-1024x1024.jpg" /> <br><h3>Here you can find👇</h3> <p>https://health-and-fitness-api.onrender.com/api/v1/workouts</p>`);    
+    res.send(`<h2> Le API response..404</h2> <img height=222 width=222 src="https://memeheist.com/wp-content/uploads/2023/08/Hum-Pe-To-Hai-Hi-No-Meme-1-1024x1024.jpg" /> <br><h3>Here you can find👇</h3> <p>https://health-and-fitness-rvrr.onrender.com/api/v1/workouts</p>`);    
 })
